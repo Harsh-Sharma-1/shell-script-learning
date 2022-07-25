c39a5125adb06b02ffb1e73a943902f6485d7718
harsh sharma <harsh112200harsh@gmail.com>
2022-07-25 15:31:06 +0530

    conditions and variables

--- setup.sh
+++ setup.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-echo "Reading user input"
+echo "Setup and configure server"
 
-read -p "Please enter your password: " user_pwd
+file_name=config.yaml
 
-echo "thanks for your password $user_pwd"
+config_dir=$1
+
+if [ -d "$config_dir" ]
+then
+        echo "reading config directory contents"
+        config_files=$(ls "$config_dir")
+else
+        echo "the config directory not fouch creating one"
+        mkdir "$config_dir"
+        touch "$config_dir/config.sh"
+fi
+
+user_group=$2
+
+if [ "$user_group" == "harsh"  ]
+then
+        echo "configure the server"
+elif [ "$user_group" == "admin"  ]
+then
+        echo "administer the server"
+else
+        echo "no permission to configure server. wrong user group"
+fi
+
+echo "using file $file_name to configure something"
+echo "here are all configuration files: $config_files"
