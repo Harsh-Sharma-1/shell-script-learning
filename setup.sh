f8b2c8e262498984d10c47fe730a429685c1145b
harsh sharma <harsh112200harsh@gmail.com>
2022-07-25 15:14:53 +0530

    variables and conditionals

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Setup and configure server"
+
+file_name=config.yaml
+
+config_dir=$1
+
+if [ -d "$config_dir" ]
+then
+	echo "reading config directory contents"
+	config_files=$(ls "$config_dir")
+else
+	echo "the config directory not fouch creating one"
+	mkdir "$config_dir"
+	touch "$config_dir/config.sh"
+fi
+
+user_group=$2
+
+if [ "$user_group" == "harsh"  ]
+then
+	echo "configure the server"
+elif [ "$user_group" == "admin"  ]
+then
+	echo "administer the server"
+else 
+	echo "no permission to configure server. wrong user group"
+fi
+
+echo "using file $file_name to configure something"
+echo "here are all configuration files: $config_files"
