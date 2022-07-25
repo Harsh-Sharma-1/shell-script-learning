4fa4aa3d9127040396c39cde46627e5b2109687a
harsh sharma <harsh112200harsh@gmail.com>
2022-07-25 16:28:26 +0530

    done

--- setup.sh
+++ setup.sh
@@ -3,5 +3,26 @@
 echo "all params $*"
 echo "number of params: $#"
 
-echo "user $1"
-echo "group $2"
+for param in $*
+ do
+	if [ -d "$param" ]
+	then
+		echo "executing scripts in the config folder"
+		ls -l "$param"
+	fi	
+ 	echo $param
+ done
+
+sum=0
+while true
+ do
+ 	read -p "enter a score : " score
+
+	if [ "$score" == "q" ]
+	then
+		echo "total score: $sum"
+		break
+	fi
+	sum=$(($sum+$score))
+		
+ done	 
